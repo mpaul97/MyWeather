07fb75b928594e3fd3958863241506e029e69981
mpaul97 <michaelandrewpaul97@gmail.com>
2023-09-02 12:24:07 -0500

    api info

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+
+const API_KEY = '9fa9382f92msh7a31d64d5592c88p168c3djsn1401d40dbe5c';
+const API_HOST = 'weatherapi-com.p.rapidapi.com';
+// const url = 'https://weatherapi-com.p.rapidapi.com/current.json?q=Los%20Angeles%20US';
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': '9fa9382f92msh7a31d64d5592c88p168c3djsn1401d40dbe5c',
+// 		'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
+// 	}
+// };
+
+// try {
+// 	const response = await fetch(url, options);
+// 	const result = await response.text();
+// 	console.log(result);
+// } catch (error) {
+// 	console.error(error);
+// }
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': API_KEY,
+            'X-RapidAPI-Host': API_HOST
+        }
+    };
+    const url = 'https://weatherapi-com.p.rapidapi.com/current.json?q=Los%20Angeles%20US';
+    useEffect(() => {
+        fetch(url, options)
+            .then(res => res.json())
+            .then(data => console.log(data))
+            .catch(error => console.error(error))
+    }, [])
+    return (
+        <div className="App">
+            
+        </div>
+    );
 }
 
 export default App;
